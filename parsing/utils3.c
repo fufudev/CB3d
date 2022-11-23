1d289fcbaad90b2c7a5880239b72918d3abcfefd
Ange <anggonza@student.42nice.fr>
2022-11-23 09:54:51 +0100

    check south pos player

--- parsing/utils3.c
+++ parsing/utils3.c
@@ -6,7 +6,7 @@
 /*   By: anggonza <anggonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 21:50:32 by anggonza          #+#    #+#             */
-/*   Updated: 2022/11/23 09:47:45 by anggonza         ###   ########.fr       */
+/*   Updated: 2022/11/23 09:54:03 by anggonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ void	init_positionn(t_data *data, int i, int j)
 	data->pos_x = j;
 	data->player_y = i * 64 + 32;
 	data->player_x = j * 64 + 32;
-	if (data->pos_y > 0)
+	if (data->pos_y > 0 && data->pos_y < (int)ft_strlen2d(data->s_map))
 	{
-		if (data->pos_x > ft_strlen_int(data->s_map[data->pos_y - 1]))
+		if (data->pos_x >= ft_strlen_int(data->s_map[data->pos_y - 1]))
+			ft_msg_free(data->map, "Player out of map\n");
+		if (data->pos_x >= ft_strlen_int(data->s_map[data->pos_y + 1]))
 			ft_msg_free(data->map, "Player out of map\n");
 	}
 	data->indic = 1;
