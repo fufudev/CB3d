011325245b575453b4cac6af14010f09c7f17f0a
Ange <anggonza@student.42nice.fr>
2022-11-23 09:48:28 +0100

    test pos player

--- parsing/utils3.c
+++ parsing/utils3.c
@@ -6,7 +6,7 @@
 /*   By: anggonza <anggonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 21:50:32 by anggonza          #+#    #+#             */
-/*   Updated: 2022/11/17 21:54:37 by anggonza         ###   ########.fr       */
+/*   Updated: 2022/11/23 09:47:45 by anggonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ void	init_positionn(t_data *data, int i, int j)
 	data->pos_x = j;
 	data->player_y = i * 64 + 32;
 	data->player_x = j * 64 + 32;
+	if (data->pos_y > 0)
+	{
+		if (data->pos_x > ft_strlen_int(data->s_map[data->pos_y - 1]))
+			ft_msg_free(data->map, "Player out of map\n");
+	}
 	data->indic = 1;
 }
 
